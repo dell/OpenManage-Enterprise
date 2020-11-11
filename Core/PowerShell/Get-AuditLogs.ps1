bbc3539b3511679dd0e400a4dd5a828d9c453912
Grant Curell <grantcurell@gmail.com>
2020-11-11 07:44:40 -0500

    #54 Get Audit Logs
    - Resolves #54 "Need a script to get audit logs and purge them weekly"
    - Adds Get-AuditLogs.ps1 and get_audit_logs.py
    - I did not see a way to purge the logs from the server. This may need to be resolved separately.
    
    Signed-off-by: Grant Curell <grant_curell@dell.com>

---
+++ Core/PowerShell/Get-AuditLogs.ps1
@@ -0,0 +1,159 @@
+<#
+_author_ = Grant Curell <grant_curell@dell.com>
+_version_ = 0.1
+
+Copyright (c) 2020 Dell EMC Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+  .SYNOPSIS
+    Retrieves the audit logs from a target OME instance and can either save them in an CSV on a fileshare or 
+    print them to screen.
+
+  .DESCRIPTION
+
+    It performs X-Auth with basic authentication. Note: Credentials are not stored on disk.
+
+  .PARAMETER IpAddress
+    This is the IP address of the OME Appliance
+  .PARAMETER Credentials
+    Credentials used to talk to the OME Appliance
+  .PARAMETER Share
+    (optional) The name of the share you would like to mount
+  .PARAMETER SmbCreds
+    (optional) The name of the SMB credentials for the share
+
+  .EXAMPLE
+    $cred1 = Get-Credentials
+    $cred2 = Get-Credentials
+    python get_audit_logs.py -IpAddress 192.168.1.5 -Credentials $cred1 -Share \\192.168.1.7\gelante -SmbCreds $cred2
+#>
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory)]
+  [System.Net.IPAddress] $IpAddress,
+
+  [Parameter(Mandatory)]
+  [pscredential] $Credentials,
+
+  [string] $Share = $null,
+
+  [pscredential] $SmbCreds = $null
+
+)
+
+
+function Get-Data {
+  <#
+    .SYNOPSIS
+      Used to interact with API resources
+
+    .DESCRIPTION
+      This function retrieves data from a specified URL. Get requests from OME return paginated data. The code below
+      handles pagination. This is the equivalent in the UI of a list of results that require you to go to different
+      pages to get a complete listing.
+
+    .PARAMETER Url
+        The API url against which you would like to make a request
+
+    .PARAMETER Filter
+        An optional parameter for providing an odata filter to run against the API endpoint.
+
+    .INPUTS
+        None. You cannot pipe objects to Get-Data.
+
+    .OUTPUTS
+        list. The Get-Data function returns a list of hashtables with the headers resulting from authentication against the OME server
+
+  #>
+  
+  [CmdletBinding()]
+  param (
+
+    [Parameter(Mandatory)]
+    [string] 
+    # The API url against which you would like to make a request
+    $Url,
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    # (Optional) A filter to run against the API endpoint
+    $Filter
+  )
+
+  $Data = @()
+  $NextLinkUrl = $null
+  $BaseUri = "https://$($IpAddress)"
+  try {
+
+    if ($PSBoundParameters.ContainsKey('Filter')) {
+      $CountData = Invoke-RestMethod -Uri $Url"?`$filter=$($Filter)" -UseBasicParsing -Method Get -Credential $Credentials -ContentType $Type -SkipCertificateCheck  
+
+      if ($CountData.'@odata.count' -lt 1) {
+        Write-Error "No results were found for filter $($Filter)."
+        return $null
+      } 
+    }
+    else {
+      $CountData = Invoke-RestMethod -Uri $Url -UseBasicParsing -Method Get -Credential $Credentials -ContentType $Type -SkipCertificateCheck  
+    }
+
+    $Data += $CountData.'value'
+    if ($CountData.'@odata.nextLink') {
+      $NextLinkUrl = $BaseUri + $CountData.'@odata.nextLink'
+    }
+    while ($NextLinkUrl) {
+      $NextLinkData = Invoke-RestMethod -Uri $NextLinkUrl -UseBasicParsing -Method Get -Credential $Credentials -ContentType $Type -SkipCertificateCheck
+      $Data += $NextLinkData.'value'
+      if ($NextLinkData.'@odata.nextLink') {
+        $NextLinkUrl = $BaseUri + $NextLinkData.'@odata.nextLink'
+      }
+      else {
+        $NextLinkUrl = $null
+      }
+    }
+    
+    return $Data
+
+  }
+  catch [System.Net.Http.HttpRequestException] {
+    Write-Error "There was a problem connecting to OME. Did it become unavailable?"
+    return $null
+  }
+
+}
+
+try {
+
+  $AuditLogs = Get-Data "https://$($IpAddress)/api/ApplicationService/AuditLogs"
+  
+  if ($null -eq $AuditLogs) {
+    Write-Output "Error: We were unable to fetch the audit logs... exiting."
+    Exit
+  }
+
+  if ($PSBoundParameters.ContainsKey('Share')) {
+    $Share = $Share.TrimEnd('\')
+    New-SmbMapping -RemotePath $Share -Username $Credentials.UserName -Password $Credentials.Password
+    $AuditLogs | Export-Csv -LiteralPath "$($Share)\$(get-date -f yyyy-MM-dd).csv"
+  }
+  else {
+    Write-Output $AuditLogs
+  }
+
+}
+catch {
+  Write-Error "Exception occured - $($_.Exception.Message)"
+}
