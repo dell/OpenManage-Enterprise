c793cecc5eb57fd24a9cbd20df565939f2de0d3f
Trevor Squillario <Trevor.Squillario@Dell.com>
2021-02-01 10:24:44 -0700

    Revert "MX python scripts"
    - M
    - This reverts commit a95012031f2ec36886c7d21266dbc228b03ddf32.
    
    Signed-off-by: Grant Curell <grant_curell@dell.com>

--- Core/Python/get_identitypool_usage.py
+++ Core/Python/get_identitypool_usage.py
@@ -24,12 +24,11 @@ Script to get the list of virtual addresses in an Identity Pool
 #### Description
 This script uses the OME REST API to get a list of virtual addresses in an Identity Pool.
 Will export to a CSV file called IdentityPoolUsage.csv in the current directory.
-
 For authentication X-Auth is used over Basic Authentication
 Note that the credentials entered are not stored to disk.
 
-#### Example
-```
+#### Python Example
+```bash
 python get_identitypool_usage.py --ip <xx> --user <username> --password <pwd>
 python get_identitypool_usage.py --ip <xx> --user <username> --password <pwd> --id 11
 python get_identitypool_usage.py --ip <xx> --user <username> --password <pwd> --id 11 --outfile "/tmp/temp.csv"
@@ -53,7 +52,8 @@ session_auth_token = {}
 
 
 def get_session(ip_address, user_name, password):
-    session_url = 'https://%s/api/SessionService/Sessions' % (ip_address)
+    session_url = 'https://%s/api/SessionService/Sessions' % ip_address
+    base_uri = 'https://%s' % ip_address  # TODO - This is not in use?
     headers = {'content-type': 'application/json'}
     user_details = {'UserName': user_name,
                     'Password': password,
@@ -74,101 +74,121 @@ def get_session(ip_address, user_name, password):
 def delete_session(ip_address, headers, id):
     session_url = "https://%s/api/SessionService/Sessions('%s')" % (ip_address, id)
     session_info = requests.delete(session_url, verify=False, headers=headers)
-    if session_info.status_code == 204:
+    if session_info.status_code == 201:
         return True
-    else: 
-        print ("Unable to delete session %s" % id)
+    else:
         return False
 
-def get_identitypool_usage(base_uri, headers, user_name, password, identitypool_id, outfile):
-    next_link_url = None
-    # Display Identity Pools
-    identitypool_url = base_uri + '/api/IdentityPoolService/IdentityPools'
-    identitypool_response = requests.get(identitypool_url, headers=headers, verify=False)
-    if identitypool_response.status_code == 200:
-        identitypool_data = identitypool_response.json()
-        identitypool_data = identitypool_data['value']
-        for i in identitypool_data:
-            print("Id: %s, Name: %s" %(i["Id"], i["Name"]))
-    else:
-        print("Unable to retrieve device list from %s" % (ip_address))
 
-    if identitypool_id == None:
-        identitypool_id_prompt = input("Please Enter Identity Pool Id: ")
-        identitypool_id = identitypool_id_prompt
+def get_device_list(ip_address, user_name, password, identitypool_id, outfile):
+    """ Authenticate with OME and enumerate devices """
+    try:
+        base_uri = 'https://%s' % ip_address
+        next_link_url = None  # TODO - This is not in use?
+        headers = {'content-type': 'application/json'}
 
-    # Get Identity Pool Usage Sets
-    identitypool_usageset_url = base_uri + "/api/IdentityPoolService/IdentityPools(%s)/UsageIdentitySets" % (identitypool_id)
-    identitypool_usageset_response = requests.get(identitypool_usageset_url, headers=headers, verify=False)
-    if identitypool_usageset_response.status_code == 200:
-        identitypool_usageset_data = identitypool_usageset_response.json()
-        identitypool_usageset_data = identitypool_usageset_data['value']
-        
-        detailentries = []
-        for usageset in identitypool_usageset_data:
-            usageset_id = usageset["IdentitySetId"]
-            usageset_name = usageset["Name"]
+        auth_token = get_session(ip_address, user_name, password)
+        if auth_token is not None:
+            headers['X-Auth-Token'] = auth_token['token']
 
-            identitypool_usageset_detail_url = base_uri + "/api/IdentityPoolService/IdentityPools(%s)/UsageIdentitySets(%s)/Details" % (identitypool_id, usageset_id)
-            identitypool_usageset_detail_response = requests.get(identitypool_usageset_detail_url, headers=headers, verify=False)
-            if identitypool_usageset_detail_response.status_code == 200:
-                identitypool_usageset_detail_json = identitypool_usageset_detail_response.json()
-                identitypool_usageset_detail_data = identitypool_usageset_detail_json['value']
-                for detailentry in identitypool_usageset_detail_data:
-                    entry = {
-                        "IdentityType": usageset_name,
-                        "ChassisName": detailentry["DeviceInfo"]["ChassisName"],
-                        "ServerName": detailentry["DeviceInfo"]["ServerName"],
-                        "ManagementIp": detailentry["DeviceInfo"]["ManagementIp"],
-                        "NicIdentifier": detailentry["NicIdentifier"],
-                        "MacAddress": detailentry["MacAddress"]
-                    }
-                    detailentries.append(entry)
-                
-                if '@odata.nextLink' in identitypool_usageset_detail_json:
-                    next_link_url = base_uri + identitypool_usageset_detail_json['@odata.nextLink']
-                    while next_link_url:
-                        next_link_response = requests.get(next_link_url, headers=headers, verify=False)
-                        if next_link_response.status_code == 200:
-                            next_link_json = next_link_response.json()
-                            next_link_json_data += next_link_json['value']
-                            for detailentry in next_link_json_data:
-                                entry = {
-                                    "IdentityType": usageset_name,
-                                    "ChassisName": detailentry["DeviceInfo"]["ChassisName"],
-                                    "ServerName": detailentry["DeviceInfo"]["ServerName"],
-                                    "ManagementIp": detailentry["DeviceInfo"]["ManagementIp"],
-                                    "NicIdentifier": detailentry["NicIdentifier"],
-                                    "MacAddress": detailentry["MacAddress"]
-                                }
-                                detailentries.append(entry)
-                            
-                            if '@odata.nextLink' in next_link_json:
-                                next_link_url = base_uri + next_link_json['@odata.nextLink']
-                            else:
-                                next_link_url = None
-                        else:
-                            next_link_url = None
-                            print("Unable to retrieve items from nextLink %s" % (next_link_url))
-        
-        # Export results to CSV
-        if (len(detailentries) > 0):
-            currentDirectory = os.path.dirname(os.path.abspath(__file__))
-            if outfile == None:
-                outFilePath = currentDirectory + os.path.sep + "IdentityPoolUsage.csv"                        
+            # Display Identity Pools
+            identitypool_url = base_uri + '/api/IdentityPoolService/IdentityPools'
+            identitypool_response = requests.get(identitypool_url, headers=headers, verify=False)
+            if identitypool_response.status_code == 200:
+                identitypool_data = identitypool_response.json()
+                identitypool_data = identitypool_data['value']
+                for i in identitypool_data:
+                    print("Id: %s, Name: %s" % (i["Id"], i["Name"]))
             else:
-                outFilePath = outfile                        
-            outputFile = open(outFilePath, 'w') 
-            output = csv.writer(outputFile) 
-            output.writerow(detailentries[0].keys()) 
-            for row in detailentries:
-                output.writerow(row.values())
-            print("Exported data to %s" %(outFilePath))
-            outputFile.close()
+                print("Unable to retrieve device list from %s" % ip_address)
+
+            if identitypool_id is None:
+                identitypool_id_prompt = input("Please Enter Identity Pool Id: ")
+                identitypool_id = identitypool_id_prompt
+
+            # Get Identity Pool Usage Sets
+            identitypool_usageset_url = base_uri + "/api/IdentityPoolService/IdentityPools(%s)/UsageIdentitySets" % (
+                identitypool_id)
+            identitypool_usageset_response = requests.get(identitypool_usageset_url, headers=headers, verify=False)
+            if identitypool_usageset_response.status_code == 200:
+                identitypool_usageset_data = identitypool_usageset_response.json()
+                identitypool_usageset_data = identitypool_usageset_data['value']
+
+                detailentries = []
+                for usageset in identitypool_usageset_data:
+                    usageset_id = usageset["IdentitySetId"]
+                    usageset_name = usageset["Name"]
+
+                    identitypool_usageset_detail_url = base_uri + \
+                                                       "/api/IdentityPoolService/IdentityPools(%s)/UsageIdentitySets(%s)/Details" \
+                                                       % (identitypool_id, usageset_id)
+                    identitypool_usageset_detail_response = requests.get(identitypool_usageset_detail_url,
+                                                                         headers=headers, verify=False)
+                    if identitypool_usageset_detail_response.status_code == 200:
+                        identitypool_usageset_detail_json = identitypool_usageset_detail_response.json()
+                        identitypool_usageset_detail_data = identitypool_usageset_detail_json['value']
+                        for detailentry in identitypool_usageset_detail_data:
+                            entry = {
+                                "IdentityType": usageset_name,
+                                "ChassisName": detailentry["DeviceInfo"]["ChassisName"],
+                                "ServerName": detailentry["DeviceInfo"]["ServerName"],
+                                "ManagementIp": detailentry["DeviceInfo"]["ManagementIp"],
+                                "NicIdentifier": detailentry["NicIdentifier"],
+                                "MacAddress": detailentry["MacAddress"]
+                            }
+                            detailentries.append(entry)
+
+                        if '@odata.nextLink' in identitypool_usageset_detail_json:
+                            next_link_url = base_uri + identitypool_usageset_detail_json['@odata.nextLink']
+                            while next_link_url:
+                                next_link_response = requests.get(next_link_url, headers=headers, verify=False)
+                                if next_link_response.status_code == 200:
+                                    next_link_json = next_link_response.json()
+                                    next_link_json_data += next_link_json['value']
+                                    for detailentry in next_link_json_data:
+                                        entry = {
+                                            "IdentityType": usageset_name,
+                                            "ChassisName": detailentry["DeviceInfo"]["ChassisName"],
+                                            "ServerName": detailentry["DeviceInfo"]["ServerName"],
+                                            "ManagementIp": detailentry["DeviceInfo"]["ManagementIp"],
+                                            "NicIdentifier": detailentry["NicIdentifier"],
+                                            "MacAddress": detailentry["MacAddress"]
+                                        }
+                                        detailentries.append(entry)
+
+                                    if '@odata.nextLink' in next_link_json:
+                                        next_link_url = base_uri + next_link_json['@odata.nextLink']
+                                    else:
+                                        next_link_url = None
+                                else:
+                                    next_link_url = None
+                                    print("Unable to retrieve items from nextLink %s" % next_link_url)
+
+                # Export results to CSV
+                if len(detailentries) > 0:
+                    current_directory = os.path.dirname(os.path.abspath(__file__))
+                    if outfile is None:
+                        out_file_path = current_directory + os.path.sep + "IdentityPoolUsage.csv"
+                    else:
+                        out_file_path = outfile
+                    output_file = open(out_file_path, 'w')
+                    output = csv.writer(output_file)
+                    output.writerow(detailentries[0].keys())
+                    for row in detailentries:
+                        output.writerow(row.values())
+                    print("Exported data to %s" % out_file_path)
+                    output_file.close()
+                else:
+                    print("No data to display")
+            else:
+                print("Unable to retrieve list from %s" % ip_address)
         else:
-            print("No usage data found")
-    else:
-        print("Unable to retrieve list from %s" % (ip_address))
+            print("Unable to create a session with appliance %s" % ip_address)
+    except Exception as error:
+        print("Unexpected error:", str(error))
+    finally:
+        delete_session(ip_address, headers, auth_token['id'])
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
@@ -179,23 +199,11 @@ if __name__ == '__main__':
                         help="Password for OME Appliance")
     parser.add_argument("--id", required=False,
                         help="Identity Pool Id")
-    parser.add_argument("--out-file", "-o", required=False,
+    parser.add_argument("--outfile", "-o", required=False,
                         help="Full path to CSV file")
 
-    ARGS = parser.parse_args()
-    base_uri = 'https://%s' %(ARGS.ip)
-    auth_token = get_session(ARGS.ip, ARGS.user, ARGS.password)
-    headers = {'content-type': 'application/json'}
-    if auth_token.get('token') != None:
-        headers['X-Auth-Token'] = auth_token['token']
-    else:
-        print("Unable to create a session with appliance %s" % (base_uri))
-        quit()
+    args = parser.parse_args()
+    if not args.password:
+        args.password = getpass()
 
-    try: 
-        if ARGS.out_file:
-            get_identitypool_usage(base_uri, headers, ARGS.user, ARGS.password, ARGS.id, ARGS.out_file)
-    except Exception as e:
-        print(traceback.format_exc())
-    finally:
-        delete_session(ARGS.ip, headers, auth_token['id'])
+    get_device_list(args.ip, args.user, args.password, args.id, args.outfile)
